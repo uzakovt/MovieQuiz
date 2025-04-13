bc6c73f61c37d7310f2e30166add288a69204043
User <User@DESKTOP-J1VGTRG>
2025-04-13 19:51:58 +0400

    AI code improvements from CodeHelper

--- MovieQuiz/Models/QuizResultsViewModel.swift
+++ MovieQuiz/Models/QuizResultsViewModel.swift
@@ -2,6 +2,6 @@ import Foundation
 
 struct QuizResultsViewModel {
     let title: String
-    let text: String
-    let buttonText: String
+    let description: String
+    let actionButtonTitle: String
 }

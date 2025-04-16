d61b61c3729ba27126c03939f414555e6f0a5f76
User <User@DESKTOP-J1VGTRG>
2025-04-17 01:04:50 +0400

    AI code improvements from CodeHelper

--- MovieQuiz/Helpers/Array+Extensions.swift
+++ MovieQuiz/Helpers/Array+Extensions.swift
@@ -2,6 +2,6 @@ import Foundation
 
 extension Array {
     subscript(safe index: Index) -> Element? {
-        indices ~= index ? self[index] : nil
+        return indices.contains(index) ? self[index] : nil
     }
 }
